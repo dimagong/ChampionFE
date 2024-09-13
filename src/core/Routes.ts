dcd984da121962e318a4dbe6a5b5534e6bfcfce5
dimagong <dimagong@gmail.com>
2024-09-13 18:01:11 +0200

    maintain: add react-native video

--- src/core/Routes.ts
+++ src/core/Routes.ts
@@ -2,5 +2,11 @@ export enum Routes {
     HOME = 'home', 
     SHOP = 'shop', 
     USER = 'user', 
-    VIDEO = 'video'
+    VIDEO = 'video',
+    VIDEO_LIST = 'videolist',
+    BEST_MOMENTS = 'bestMomemts', 
+    ARTICLES = 'articles', 
+    TEAM = 'team', 
+    STATS = 'stats', 
+    MAIN = 'main'
 }
