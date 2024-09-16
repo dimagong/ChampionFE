dd93b600f54a3d57a620ae4c091b729356a79df3
dimagong <dimagong@gmail.com>
2024-09-16 18:10:30 +0200

    maintain(interfaces): update video interfaces

--- src/interfaces/interfacesVideoImages.ts
+++ src/interfaces/interfacesVideoImages.ts
@@ -4,3 +4,11 @@ export interface IVideoImages {
     name: Maybe<Scalars['String']>,
     url: Maybe<Scalars['String']>,
 }
+
+export interface IAnnounceVideoCard {
+    name: string
+    player: string
+    team: string
+    imageUrl: string
+    videoUrl: string
+}
