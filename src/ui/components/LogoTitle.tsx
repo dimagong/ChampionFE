f8229b119fcc4f14c0579634c5b8f58fa2b51360
dimagong <dimagong@gmail.com>
2025-03-21 18:21:58 +0100

    change mail logo

--- src/ui/components/LogoTitle.tsx
+++ src/ui/components/LogoTitle.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import {Image} from 'react-native';
+import React from 'react'
+import {Image} from 'react-native'
 
 export const LogoTitle = () => {
-  return (
-    <Image
-      style={{width: 30, height: 30}}
-      source={require('../images/stiavnica.webp')}
-    />
-  );
-};
+    return (
+        <Image
+            style={{width: 50, height: 50}}
+            // source={require('../images/stiavnica.webp')}
+            source={require('../premierLeagueIcons/lfc-digital-crest-2024.webp')}
+        />
+    )
+}
