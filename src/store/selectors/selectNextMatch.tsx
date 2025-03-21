dee26ae75d700df6977a312faba60e244c671261
dimagong <dimagong@gmail.com>
2025-03-21 18:02:32 +0100

    adjust data fir display next matches

--- src/store/selectors/selectNextMatch.tsx
+++ src/store/selectors/selectNextMatch.tsx
@@ -1,27 +1,34 @@
-import {createSelector} from '@reduxjs/toolkit';
-import type {RootState} from '../store';
+import {createSelector} from '@reduxjs/toolkit'
+import type {RootState} from '../store'
+
+interface INextMatch {
+    guestTeam: string
+    homeTeam: string
+    score: string
+    time: number
+}
+const optionsTime: Intl.DateTimeFormatOptions = {
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    hour12: false,
+    minute: '2-digit',
+}
 
 export const selectNextMatch = createSelector(
-  (state: RootState) => state.nextMatches,
-  value => {
-    const teamsDetails = value.value[0] as any;
-    const teamHome = teamsDetails?.teams[0].name ?? 'No team';
-    const teamGuest = teamsDetails?.teams[1].name ?? 'No team';
-    // const options: any = {year: '2-digit', month: 'numeric', day: 'numeric'};
-    const optionsTime: any = {
-      year: '2-digit',
-      month: 'numeric',
-      day: 'numeric',
-      hour: '2-digit',
-      hour12: false,
-      minute: '2-digit',
-    };
-
-    const time =
-      new Date(teamsDetails?.startDate).toLocaleDateString(
-        'en-US',
-        optionsTime,
-      ) ?? new Date().toLocaleDateString('en-US', optionsTime);
-    return {teamHome, teamGuest, time};
-  },
-);
+    (state: RootState) => state.nextMatches,
+    value => {
+        const teamsDetails: INextMatch = value.value[0]
+        const time =
+            new Date(teamsDetails?.time).toLocaleDateString(
+                'en-US',
+                optionsTime,
+            ) ?? new Date().toLocaleDateString('en-US', optionsTime)
+        return {
+            teamHome: teamsDetails?.homeTeam ?? 'No team',
+            teamGuest: teamsDetails?.guestTeam ?? 'No team',
+            time,
+        }
+    },
+)
--- src/store/thunks/fetchNextMatches.ts
+++ src/store/thunks/fetchNextMatches.ts
@@ -1,10 +1,10 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import {receiveNextMatches} from '../../data/api/services'
 
-export const fetchNextMatches = createAsyncThunk(  
-  'nextMatches/fetchNextMatches',
-  async (thunkAPI) => {
-    const data = await receiveNextMatches()
-    return data
-  }
+export const fetchNextMatches = createAsyncThunk(
+    'nextMatches/fetchNextMatches',
+    async thunkAPI => {
+        const data = await receiveNextMatches()
+        return data
+    },
 )
