4b6d9713723284ae788ca1858df76ec31e14b816
dimagong <dimagong@gmail.com>
2025-03-21 18:01:12 +0100

    set library for parse html  articles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import type {ReactNode} from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 //import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
@@ -9,7 +8,6 @@ import {Provider as ReduxProvider} from 'react-redux'
 import {Provider as PaperProvider} from 'react-native-paper'
 import {RootScreen} from './src/screens'
 import {theme} from '@ui/theme/theme'
-//() => ReactNode
 
 const App = () => {
     return (
